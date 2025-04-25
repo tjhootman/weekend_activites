d1805b639cba73b22a9379fd993f326988456b27
tjhootman <tjhootman@gmail.com>
2025-04-24 22:50:31 -0400

    Add pastel colors to pie chart, add title and docmentation

--- weekend_activities.py
+++ weekend_activities.py
@@ -1,12 +1,22 @@
 import matplotlib.pyplot as plt
 
+# dictionary containing activities and time spent
 activity_time = {'Sleeping': 9, 'Homework': 2, 'Playing Games': 3, 'Reading': 1, 'Eating': 2, 
               'With Friends': 4, 'Other': 3}
 
+# extract activites and time spent
 activities = activity_time.keys()
 time_spent = activity_time.values()
 
-plt.figure()
-plt.pie(time_spent, labels=activities, autopct='%1.1f%%',startangle=90)
+# pastel color options for pie chart
+pastel_colors = ["#FFADAD", "#FFD6A5", "#FDFFB6", "#CAFFBF", "#9BF6FF", "#A0C4FF", "#BDB2FF"]
 
+# create the pie chart
+plt.figure(figsize=(8,8))
+plt.pie(time_spent, labels=activities, autopct='%1.1f%%', startangle=90, colors=pastel_colors)
+
+# add the title
+plt.title('How I Spent My Saturday')
+
+# show the plot
 plt.show()
