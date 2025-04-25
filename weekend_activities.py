4ccac573456adbd47508be0917afe9f7fb20c209
tjhootman <tjhootman@gmail.com>
2025-04-24 22:29:56 -0400

    Initial setup of files. Create dictionary and extract keys and values

---
+++ weekend_activities.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+activity_time = {'Sleeping': 9, 'Homework': 2, 'Playing Games': 3, 'Reading': 1, 'Eating': 2, 
+              'With Friends': 4, 'Other': 3}
+
+activities = activity_time.keys()
+time_spent = activity_time.values()
